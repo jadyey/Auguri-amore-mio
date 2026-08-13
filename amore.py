3fbfcc41d9c7297c3d1bd0b49397d2f11725cc4e
jadyey <giadaventiquattro@gmail.com>
2026-08-13 13:55:22 +0200

    Update amore.py

--- amore.py
+++ amore.py
@@ -1,10 +1,16 @@
 import streamlit as st
 st.set_page_config(page_title="Buon Compleanno tato 🩷", page_icon= "🎂")
-st.title("Tanti auguri amore mio 💕")
+st.title("Tanti auguri amore mio 💕  🎂🎂🎂")
 #canzone st.audio("")
 st.divider()
 st.markdown("""
-### Ciao Amore
+### Ciao Amore,
+Oggi compi 22 anni, ormai sei un uomo adulto, e sono davvero contenta di sapere che ho di fianco a me una persona fantastica come te.
+Sono fiera di te, di quello che sei e quello che diventarai. Non vedo l'ora di passare la vita con te, vederti crescere con me sarebbe un sogno.
+Immagino le domeniche a riposarci insieme, a coccolarci, ma anche fare la spesa, pulire la casa insieme.
+Immagino le sere dopo le lunghe giornate a lavoro, ad abbracciarci, a consolarci e soprattutto a ridere insieme.
+Spero di poter avere il privilegio di passare la mia vita con te.
+Ti auguro il meglio amore mio
 **Ti amo**
 *la tua Giada*
 """)
